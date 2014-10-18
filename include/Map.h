9bf68cdceaa9a15b625fd369b82d4d20246f1f2f
Lucas Wiener <lucas@wiener.se>
2014-10-18 20:33:29 +0200

    Now creating map matrix

---
+++ include/Map.h
@@ -0,0 +1,61 @@
+#ifndef __MAP_H
+#define __MAP_H
+
+#include <vector>
+#include <stdexcept>
+
+namespace s8 {
+    class Map {
+        class Row {
+            const size_t cols;
+            std::vector<int> row;
+
+        public:
+            Row(size_t cols, size_t default_value) : cols(cols) {
+                row = std::vector<int>(cols, default_value);
+            }
+
+            size_t num_cols() const {
+                return cols;
+            }
+
+            int & operator[] (size_t index) throw(std::out_of_range) {
+                return row.at(index);
+            }
+        };
+
+    size_t rows;
+    size_t cols;
+
+    std::vector<Row> matrix;
+
+    public:
+        Map() : rows(0), cols(0) {}
+
+        Map(size_t rows, size_t cols, int default_value = 0) : rows(rows), cols(cols) {
+            matrix = std::vector<Row>(rows, Row(cols, default_value));
+            ROS_INFO("%ldx%ld", rows, cols);
+        }
+
+        Map & operator= (Map && map) {
+            rows = map.rows;
+            cols = map.cols;
+            matrix = std::move(map.matrix);
+            return *this;
+        }
+
+        size_t num_rows() const {
+            return rows;
+        }
+
+        size_t num_cols() const {
+            return cols;
+        }
+
+        const Row & operator[] (size_t index) throw (std::out_of_range) {
+            return matrix.at(index);
+        }
+    };
+}
+
+#endif
--- src/mapper.cpp
+++ src/mapper.cpp
@@ -1,17 +1,51 @@
 #include <ros/ros.h>
 
 #include <s8_common_node/Node.h>
+#include <Map.h>
 
 #define NODE_NAME       "s8_mapper_node"
 
 #define HZ              10
 #define BUFFER_SIZE     1000
 
+#define PARAM_SIDE_LENGTH_NAME      "side_length"
+#define PARAM_SIDE_LENGTH_DEFAULT   10.0
+#define PARAM_RESOLUTION_NAME       "resolution"
+#define PARAM_RESOLUTION_DEFAULT    0.05
+
+#define CELL_UNKNOWN                -1
+#define CELL_FREE                   0
+#define CELL_OBSTACLE               0 << 1
+#define CELL_WALL                   0 << 2 & CELL_OBSTACLE
+#define CELL_OBJECT                 0 << 3 & CELL_OBSTACLE
+#define CELL_ROBOT                  0 << 4
+
 class Mapper : public s8::Node {
+    double side_length;
+    double resolution;
+    s8::Map map;
 
 public:
     Mapper() {
-        
+        init_params();
+        print_params();
+
+        double side_cells_d = side_length / resolution;
+        size_t side_cells = (size_t)side_cells_d;
+        double loss = side_cells_d - side_cells;
+
+        if(loss >= resolution) {
+            ROS_WARN("Losing cells with current side_length and resolution. Cells lost on each side: %d", (int)(loss / resolution));
+        }
+
+        map = s8::Map(side_cells, side_cells, CELL_UNKNOWN);
+
+        ROS_INFO("map size: %ldx%ld", map.num_rows(), map.num_cols());
+    }
+
+    void init_params() {
+        add_param(PARAM_SIDE_LENGTH_NAME, side_length, PARAM_SIDE_LENGTH_DEFAULT);
+        add_param(PARAM_RESOLUTION_NAME, resolution, PARAM_RESOLUTION_DEFAULT);
     }
 };
 
